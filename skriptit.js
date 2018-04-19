02098a15aaf0a94abf2e27d58115bd2010c05c73
XPocin <npaavilainen91@gmail.com>
2018-04-19 13:13:40 +0300

    Add files via upload

--- skriptit.js
+++ skriptit.js
@@ -1,1 +1,29 @@
-a
+function c-to-f{
+var lampo = document.getElementById('arvo').value;
+
+}
+
+function c-to-k{
+var lampo = document.getElementById('arvo').value;
+
+}
+
+function f-to-c{
+var lampo = document.getElementById('arvo').value;
+
+}
+
+function f-to-k{
+var lampo = document.getElementById('arvo').value;
+
+}
+
+function k-to-c{
+var lampo = document.getElementById('arvo').value;
+
+}
+
+function k-to-f{
+var lampo = document.getElementById('arvo').value;    
+
+}
